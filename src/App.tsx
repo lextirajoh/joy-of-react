3a20fba7e133feeca6deb196b1d871a3fb55d819
Lex <alext@riseup.net>
2023-01-27 23:22:38 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
 function App() {
-  return <></>;
+  function exclaim(string: string) {
+    return string + '!';
+  }
+
+  const exclaimArrow = (string: string) => string + '!';
+
+  const user = {
+    name: 'François Bouchard',
+    city: 'Saint-Louis-du-Ha! Ha!',
+    province: 'Québec',
+    country: 'Canada',
+    postalCode: 'A1B 2C3',
+  };
+
+  const { name, country } = user;
+  const fruits = ['apple', 'banana', 'cantaloupe'];
+  const [firstFruit, secondFruit] = fruits;
+  
+  console.log(firstFruit);
+
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
 }
 
 export default App;
