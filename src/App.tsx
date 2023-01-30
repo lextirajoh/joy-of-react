57ab5443a2286573799314c5125e73d2d0b4b54b
Lex <alext@riseup.net>
2023-01-30 14:47:25 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import { Avatar, Button, ContactCard, ShoppingCart } from './components';
 
 export default function App() {
-  function shoutSentence(sentence: string): string {
-    return sentence.toUpperCase();
-  }
-
   return (
     <>
-      {/* <ContactCard />
-      <Button status="cancel">Cancel</Button>
-      <Button status="confirm">Confirm</Button>
-      <Avatar /> */}
+      {/* <ContactCard /> */}
+      {/* <Button status="cancel">Cancel</Button> */}
+      {/* <Button status="confirm">Confirm</Button> */}
+      {/* <Avatar /> */}
       <ShoppingCart />
     </>
   );
 }
-//
